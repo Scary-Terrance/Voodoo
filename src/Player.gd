1048ca07b67b0298595720fbf977c3e30a22e2a8
Ben Ventura <oeslian@.com>
2020-01-12 16:45:06 -0500

    Working on wall jump, has a bug to be solved.

--- src/Player.gd
+++ src/Player.gd
@@ -1,11 +1,16 @@
 extends KinematicBody2D
 
+# Onready variables for nodes
 onready var animation_player = $AnimationPlayer
+onready var left_wall = $WallRaycasts/LeftWallRaycasts
+onready var right_wall = $WallRaycasts/RightWallRaycasts
 
 # Exports for editor
 export var gravity = 320
 export var max_jump = -224
 export var move_speed = 128
+export var wall_jump_speed = 64
+export var h_weight = 0.25
 
 # Constants
 const UP = Vector2(0, -1)
@@ -13,19 +18,38 @@ const UP = Vector2(0, -1)
 # Player variables
 var velocity = Vector2(0, 0)
 var direction = 0
+var wall_direction = 0
 var double_jump = true
-
-#onready var animation_player : AnimationPlayer = 
 
 func _apply_gravity(delta):
 	velocity.y += gravity * delta
-	
-func _apply_movement(delta):
-	_apply_gravity(delta)
-	velocity.x = lerp(velocity.x, move_speed * direction, 0.25)
+
+func _h_movement():
+	velocity.x = lerp(velocity.x, move_speed * direction, h_weight)
 	if direction == -1:
 		$Sprite.flip_h = true
 	elif direction == 1:
 		$Sprite.flip_h = false
+
+func _update_wall_direction():
+	var is_wall_left = _check_is_valid_wall(left_wall)
+	var is_wall_right = _check_is_valid_wall(right_wall)
+	if is_wall_left && is_wall_right:
+		wall_direction = direction
+	else:
+		wall_direction = -int(is_wall_left) + int(is_wall_right)
+
+# Check if a wall is able to be jumped on	
+func _check_is_valid_wall(wall_raycasts):
+	for raycast in wall_raycasts.get_children():
+		if raycast.is_colliding():
+			# acos for radians, detect a wall collision
+			var dot = acos(Vector2.UP.dot(raycast.get_collision_normal()))
+			# Check that walls arent sloped or too steep like a roof angle
+			if dot > PI * 0.35 && dot < PI * 0.55:
+				return true
+	return false
+	
+func _apply_movement():
 	velocity = move_and_slide(velocity, UP)
 
--- src/PlayerSM.gd
+++ src/PlayerSM.gd
@@ -6,6 +6,7 @@ func _ready():
 	add_state("run")
 	add_state("jump")
 	add_state("fall")
+	add_state("wall_slide")
 	call_deferred("set_state", states.idle)
 
 func _input(event):
@@ -35,11 +36,20 @@ func _input(event):
 			parent.direction += 1
 		elif event.is_action_released("right"):
 			parent.direction += -1
+			
+	elif state == states.wall_slide:
+		if event.is_action_pressed("jump"):
+			parent.velocity.y = parent.max_jump
+			parent.direction = -parent.wall_direction
+			parent.velocity.x += -parent.wall_direction * parent.move_speed
 
 # Handle all logic based on current state
 func _state_logic(delta):
+	parent._update_wall_direction()
+	parent._apply_gravity(delta)
+	parent._h_movement()
 	# Apply all movement for the player
-	parent._apply_movement(delta)
+	parent._apply_movement()
 
 # Handle logic to determine if we should change state
 func _get_transition(delta):
@@ -62,24 +72,45 @@ func _get_transition(delta):
 				return states.idle
 		states.jump:
 			if parent.is_on_floor():
-				parent.double_jump = true
 				return states.idle
+			elif parent.wall_direction != 0:
+				return states.wall_slide
 			elif parent.velocity.y >= 0:
 				return states.fall
 		states.fall:
 			if parent.is_on_floor():
-				parent.double_jump = true
 				return states.idle
+			elif parent.wall_direction != 0:
+				return states.wall_slide
 			elif parent.velocity.y < 0:
 				return states.jump
+		states.wall_slide:
+			if parent.is_on_floor():
+				return states.idle
+			elif parent.velocity.x != 0:
+				if parent.velocity.y < 0:
+					return states.jump
+				elif parent.velocity.y >= 0:
+					return states.fall
 
 func _enter_state(new_state, old_state):
 	match new_state:
 		states.idle:
+			parent.double_jump = true
 			parent.animation_player.play("idle")
 		states.run:
+			parent.double_jump = true
 			parent.animation_player.play("run")
 		states.jump:
 			parent.animation_player.play("jump")
 		states.fall:
 			parent.animation_player.play("fall")
+		states.wall_slide:
+			parent.double_jump = true
+			self.flip_h = -parent.wall_direction
+			parent.animation_player.play("idle")
+			
+func _exit_state(old_state, new_state):
+	match old_state:
+		states.wall_slide:
+			parent.direction = 0
